7f44274920ca1a8d7c07aed03c2b1420dda93750
Andrii Korshak <korshakbrvr@gmail.com>
2020-10-30 17:26:07 +0200

    lesson-1: add homework

---
+++ Lesson1/Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {         
+        static void Main(string[] args)
+        {
+            string content = "Здесь находится контент";
+
+            Book book = new Book("Автор1", "Автор2", content);
+
+            book.Show();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figure figure = new Figure(new Point("A", 2, 2), new Point("B", 4, 2), new Point("C", 3, 6), new Point("D", 10,20), new Point("E", 50, 70));
+            Console.WriteLine("{0}", figure.Type);
+            Console.Write("P = ");
+            figure.PerimeterCalculator();
+            Console.ReadKey();
+        }
+    }
+}
